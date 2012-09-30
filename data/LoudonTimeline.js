c13af4d45440e93f9cc13fbcd3ccb4c5f015a293
Doug Reside <dougreside@gmail.com>
2012-09-29 21:25:02 -0400

    Clean up SVN and added captions

--- data/LoudonTimeline.js
+++ data/LoudonTimeline.js
@@ -8,7 +8,7 @@ LoudonTimeline=
             {
                 "startDate":"1925,9,25",
                 "headline":"Childhood",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Born in Connecticut</li><li>Born in Connecticut</li><li>Born in Connecticut</li><li>Born in Connecticut</li><li>Born in Connecticut</li><li>Born in Connecticut</li><li>Born in Connecticut</li></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div><ul><p>Born in Connecticut, Dorothy Loudon lived in...</p></div>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4062443",
@@ -17,10 +17,9 @@ LoudonTimeline=
                 }
             },
             {
-                "startDate":"1946,6,1",
-                "endDate":"1962,1,1",
-                "headline":"Cabaret career",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "startDate":"1959",
+                "headline":"Tamiment Playhouse",
+                "text":"<div class=\"relatedHead\">Related items:</div><p>Loudon spent one summer at Tamiment, a resort in the Poconos whose theater company was famous for staging an original, full-length revue each week, and for nurturing young talent. Under the supervision of producer Monroe \"Moe\" Hack, Loudon worked alongside sketch writers Woody Allen and David Panich, lyricists Marshall Barer, Mary Rodgers, and Fred Ebb, performers Lenny Maxwell, Sheila Smith, Virginia Vestoff, Bill Heyer, and Bob Dishy, and director Christopher Hewett (later television\'s Mr. Belvedere).</p>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4062310",
@@ -28,12 +27,24 @@ LoudonTimeline=
                     "caption":"<a href=\"./index.html#item/12\">Scrapbook</a>"
                 }
             },
-           
+            {
+                "startDate":"1946,6,1",
+                "endDate":"1962,1,1",
+                "headline":"Cabaret career",
+                "text":"<div class=\"relatedHead\">Related items:</div><p>After signing with the top talent agency MCA, Loudon quickly became \"one of the most sought-after singers in the Bistro Belt,\" playing regular gigs at Le Ruban Bleu (at 4 E. 56th St.) and then the Blue Angel (a few blocks away at 152 E. 55th St.) as well as out-of-town engagements in Boston, Philadelphia, Cleveland, Dallas, and even Bermuda. Loudon developed a unique patter that interspersed comedic songs and monologues with straight renditions of classic numbers. [influences] Loudon recorded one album of this material, Live at the Blue Angel, for Coral Records in 1959.</p>",
+                "asset":
+                {
+                    "media":"http://images.nypl.org/index.php?t=w&id=4062310",
+                    "credit":"",
+                    "caption":"<a href=\"./index.html#item/12\">Scrapbook</a>"
+                }
+            },
+    
 			{
                 "startDate":"1962,11,10",
                 "endDate":"1962,11,17",
                 "headline":"Nowhere to Go But Up",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div><p>Loudon\'s Broadway debut was an ill-fated Prohibition-era musical comedy by James Lipton (yes, the Inside the Actors Studio host) and Sol Berkowitz.  After a disastrous Philadelphia tryout director Sidney Lumet was replaced by Mel Brooks, whose rewrites couldn\'t save it; co-star Bert Convy called it Nowhere to Throw But Up.  The New York notices weren\'t all bad (Loudon, playing a gangster\'s moll, inaugurated a tradition of being singled out for praise in otherwise mixed or bad reviews) but the show folded after a week.</p>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4061446",
@@ -45,7 +56,7 @@ LoudonTimeline=
                 "startDate":"1963,9,24",
                 "endDate":"1964,6,16",
                 "headline":"Garry Moore Show",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4061332",
@@ -57,7 +68,7 @@ LoudonTimeline=
                 "startDate":"1968,9,29",
                 "endDate":"1968,11,23",
                 "headline":"No&euml;l Coward's Sweet Potato",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div><p>English actor Roderick Cook and choreographer Lee Theodore conceived this revue of Coward songs and skits that got mixed reviews and closed after 44 performances. Amid an oddball cast that included George Grizzard, Carole Shelley, Bonnie Schon, Tom Kneebone, and a young Robert LuPone, Loudon performed a torch song (\"If Love Were All\") and a tap number with dancer Arthur Mitchell. Clive Barnes wrote that Loudon had \"just that quality of sweet, motherly bitchery that Coward so often needs.\"</p>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4061466",
@@ -69,7 +80,7 @@ LoudonTimeline=
                 "startDate":"1969,10,16",
                 "endDate":"1970,1,10",
                 "headline":"Three Men on a Horse",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div><p>Less than a year after Fig Leaves flopped, director George Abbott put Loudon into a supporting role (albeit with over-the-title billing) in this revival of Abbott and John Cecil Holm\'s racetrack comedy.  Loudon played the showgirl girlfriend of the gambler Patsy (Sam Levene reprised the role he had originated in 1935!).  The supporting cast - Jack Gilford, Hal Linden, Butterfly McQueen, Rosemary Prinz, Paul Ford - was terrific and the reviews respectable, but nostalgia was not in season and Three Men closed in three months.</p>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4061945",
@@ -81,7 +92,7 @@ LoudonTimeline=
                 "startDate":"1971,2,16",
                 "endDate":"1971,3,30",
                 "headline":"Lolita, My Love",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div><p>Alan Jay Lerner (Camelot) and John Barry, composer of the James Bond theme, adapted Vladimir Nabokov\'s novel Lolita into this musical tragicomedy. Loudon played the nymphet\'s mother, Charlotte Haze (the role essayed by Shelley Winters in the Stanley Kubrick film), and stopped the show with the number \"Sur les quais de Ramsdale, Vermont.\" Once again, Loudon worked with first-rate talent, but the ambitious production closed out of town after tryouts in Boston. Was the subject matter just too creepy for Broadway?</p>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4061262",
@@ -93,7 +104,7 @@ LoudonTimeline=
                 "startDate":"1969,1,2",
                 "endDate":"1969,1,6",
                 "headline":"The Fig Leaves are Falling",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div><p>This satire of suburban mores, in the vein of The Seven Year Itch, featured Loudon as the wife of a Larchmont commuter (Barry Nelson) who contemplates a dalliance with his hippie secretary.  The critics were brutal but almost universally spared Loudon, who had three good numbers (particularly \"All of My Laughter\") and copped a Tony nomination even though the show folded after four performances. A cult item because of the names attached - George Abbott directed, Albert Hague composed, and popular song parodist Allan Sherman did the book and lyrics - Fig Leaves also featured future pop idol David Cassidy as Loudon\'s son.</p>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4061220",
@@ -105,7 +116,7 @@ LoudonTimeline=
              "startDate":"1973,4,23",
                 "endDate":"1973,6,17",
                 "headline":"The Women",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4061967",
@@ -118,7 +129,7 @@ LoudonTimeline=
                          "startDate":"1977,4,21",
                 "endDate":"1978,8,14",
                 "headline":"Annie",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4048317",
@@ -130,7 +141,7 @@ LoudonTimeline=
                 "startDate":"1978,12,14",
                    "endDate":"1979,3,24",
                 "headline":"Ballroom",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4060979",
@@ -142,7 +153,7 @@ LoudonTimeline=
                 "startDate":"1980,3,26",
                 "endDate":"1980,6,29",
                 "headline":"Sweeney Todd",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4061871",
@@ -154,7 +165,7 @@ LoudonTimeline=
                 "startDate":"1981,11,19",
                 "endDate":"1982,3,13",
                 "headline":"The West Side Waltz",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4062070",
@@ -166,7 +177,7 @@ LoudonTimeline=
                 "startDate":"1983,12,11",
                 "endDate":"1985,1,28",
                 "headline":"Noises Off",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4061479",
@@ -178,7 +189,7 @@ LoudonTimeline=
                 "startDate":"1985,12,18",
                   "endDate":"1986,4,20",
                 "headline":"Jerry's Girls",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4061760",
@@ -190,7 +201,7 @@ LoudonTimeline=
                 "startDate":"1989,12,22",
                   "endDate":"1990,1,20",
                 "headline":"Annie 2",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4048513",
@@ -202,7 +213,7 @@ LoudonTimeline=
                 "startDate":"1971,10",
                   "endDate":"1972,2",
                 "headline":"The Effect of Gamma Rays on Man-in-the-Moon Marigolds",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4061170",
@@ -214,7 +225,7 @@ LoudonTimeline=
                 "startDate":"1988",
                   "endDate":"1989",
                 "headline":"Driving Miss Daisy",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4061076",
@@ -227,7 +238,7 @@ LoudonTimeline=
                 "startDate":"1991,7",
                 "endDate":"1991,8",
                 "headline":"The Matchmaker",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4061976",
@@ -238,7 +249,7 @@ LoudonTimeline=
             {
                 "startDate":"1992,6,10",
                 "headline":"Sondheim: A Celebration at Carnegie Hall",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4061276",
@@ -249,7 +260,7 @@ LoudonTimeline=
             {
                 "startDate":"1994",
                 "headline":"Comedy Tonight",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4061899",
@@ -260,7 +271,7 @@ LoudonTimeline=
             {
                 "startDate":"1996",
                 "headline":"Show Boat",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4061847",
@@ -271,7 +282,7 @@ LoudonTimeline=
             {
                 "startDate":"1982",
                 "headline":"Princess Grace and the Fazzaris",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4061728",
@@ -282,7 +293,7 @@ LoudonTimeline=
             {
                 "startDate":"1984",
                 "headline":"Garbo Talks",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4061307",
@@ -294,7 +305,7 @@ LoudonTimeline=
                 "startDate":"1965",
                   "endDate":"1966",
                 "headline":"Luv",
-                "text":"<div class=\"relatedHead\">Related items:</div><div class=\"relatedItems\"><ul><li>Scrapbook</li></ul></div>",
+                "text":"<div class=\"relatedHead\">Related items:</div><p>Alongside Tom Bosley and Herb Edelman, Loudon spent ten months touring in this quirky hit comedy by Murray Schisgal.  (On Broadway, the cast consisted of Anne Jackson, Eli Wallach, and Alan Arkin, under the direction of Mike Nichols, who had worked with Loudon in nightclubs and on live television.)  Loudon\'s letters from the road to her friend and housesitter Arthur Gorton offer a remarkably detailed and witty chronicle of clashing egos, unresponsive audiences, and other travails of living out of a trunk. </p>",
                 "asset":
                 {
                     "media":"http://images.nypl.org/index.php?t=w&id=4061416",
